795e006356a1131f9c0bf3eb1e33593d5d1671d5
asifalam515 <asibulalam515@gmail.com>
2025-01-10 22:13:08 +0600

    layout created

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Next Cart";
+export const APP_DETAILS =
+  process.env.NEXT_PUBLIC_APP_DETAILS ||
+  "Modern Emcommerse platfrom built with next js";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000/";
