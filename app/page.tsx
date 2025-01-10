8662e0018cef263dc9fb7c738d821fa5d7a182c6
asifalam515 <asibulalam515@gmail.com>
2025-01-10 21:48:14 +0600

    shad cn initialized

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const HomePage = () => {
-  return <div>ProStore</div>;
+  return (
+    <div>
+      <Button>Button is here</Button>
+    </div>
+  );
 };
 
 export default HomePage;
